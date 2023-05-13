3eb487853a39fb1d31dca1942b6c3edf470c4b6e
ENIOLA SODIQ OLADIPUPO <49729441+Enisco@users.noreply.github.com>
2023-05-13 03:08:14 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/chip_options_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chip_options_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen.dart
@@ -0,0 +1,97 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class ChipTextFieldScreen extends StatefulWidget {
+  const ChipTextFieldScreen({super.key});
+
+  @override
+  State<ChipTextFieldScreen> createState() => _ChipTextFieldScreenState();
+}
+
+class _ChipTextFieldScreenState extends State<ChipTextFieldScreen> {
+  TextEditingController tagsController = TextEditingController();
+  List<String> tags = [];
+
+  void saveTag(String value) {
+    print('Adding $value to tags list');
+    setState(() {
+      tags.add(value);
+      tagsController.text = '';
+      // tagsController;
+    });
+  }
+
+  void deleteTag(String value) {
+    print('Removing $value from tags list');
+    setState(() {
+      tags.remove(value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Text to Chip Field',
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: size.width * 0.9,
+          padding: const EdgeInsets.only(
+            top: 10,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+            border: Border.all(color: Colors.black12),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // const Icon(Icons.wallet),
+              tags.isNotEmpty
+                  ? GridView.builder(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(10),
+                      itemCount: tags.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        // mainAxisSpacing: 10,
+                        // crossAxisSpacing: 10,
+                        mainAxisExtent: 45,
+                      ),
+                      itemBuilder: (context, index) => Chip(
+                        label: Text(
+                          tags[index],
+                        ),
+                        padding: const EdgeInsets.all(0),
+                        onDeleted: () => deleteTag(tags[index]),
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+              SizedBox(
+                height: tags.isNotEmpty ? 30 : 0,
+              ),
+              TextFormField(
+                controller: tagsController,
+                cursorColor: Colors.teal,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  hintText: "Enter tag here",
+                ),
+                onFieldSubmitted: (value) => saveTag(value),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
